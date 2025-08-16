75b3d2e1d831853f32fbfa7eae992b9bc8242ef9
yongxin <heyongxin@live.cn>
2025-08-16 18:36:09 +0700

    Create bee.cpp

---
+++ script/bee.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+	char c = 'a';
+	int x = 5;
+	float f = 5.25;
+	double d = 15.673;
+	double y = 5.0e10;
+	
+	int numeros[5] = {1, 2, 3, 4, 5};
+	for (size_t i = 0; i < 4; i++)
+	{
+	printf("%d /n", numeros[i]);
+	}
+	printf ("O valor � c =%c \n", c);
+	printf ("O valor � x =%d \n", x);
+	printf ("O valor � f =%c \n", f);
+	printf ("O valor � d =%d \n", d);
+	printf ("O valor � y =%d \n", y);
+	}
